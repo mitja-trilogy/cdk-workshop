1a9c3a374984c592be71ea87de844c7bf058521c
Mitja Resek <mitja.resek@trilogy.com>
2023-07-27 15:29:36 +0200

    initial

---
+++ lib/lab3.ts
@@ -0,0 +1,85 @@
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as sqsSubscriptions from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as apigw from 'aws-cdk-lib/aws-apigateway';
+
+import * as path from 'path';
+import { Construct } from 'constructs';
+import * as subscription from "aws-cdk-lib/aws-sns-subscriptions";
+
+export class lab3 extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        // RidesBookingTable
+        const ridesBookingTable = new dynamodb.Table(this, 'RidesBookingTable', {
+            tableName: `RidesBooking-${this.stackName}`,
+            partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+            sortKey: { name: 'responder', type: dynamodb.AttributeType.STRING },
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        });
+
+        // RequestForQuotesTopic
+        const requestForQuotesTopic = new sns.Topic(this, 'RequestForQuotesTopic');
+
+        // RequestForQuotesResponseQueue
+        const requestForQuotesResponseQueue = new sqs.Queue(this, 'RequestForQuotesResponseQueue', {
+            visibilityTimeout: cdk.Duration.seconds(120),
+        });
+
+        // RequestForQuotesService
+        const requestForQuotesService = new lambda.Function(this, 'RequestForQuotesService', {
+            runtime: lambda.Runtime.PYTHON_3_7,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'requestForQuotesService.handler',
+            environment: {
+                TABLE_NAME: ridesBookingTable.tableName,
+                TOPIC_ARN: requestForQuotesTopic.topicArn,
+            },
+        });
+
+        ridesBookingTable.grantReadWriteData(requestForQuotesService);
+        requestForQuotesTopic.grantPublish(requestForQuotesService);
+
+        new apigw.LambdaRestApi(this, 'Endpoint', {
+            handler: requestForQuotesService,
+        });
+
+        // UnicornManagementResources (Generic Unicorn Management Service)
+
+        for (let i = 1; i <= 10; i++) {
+            const unicornManagementResource = new lambda.Function(this, `UnicornManagementResource${i}`, {
+                runtime: lambda.Runtime.PYTHON_3_7,
+                code: lambda.Code.fromAsset('lambda'),
+                handler: 'genericUnicornManagementService.handler',
+                environment: {
+                    SERVICE_NAME: `UnicornManagementResource${i}`,
+                    QUEUE_URL: requestForQuotesResponseQueue.queueUrl,
+                },
+            });
+
+            requestForQuotesResponseQueue.grantSendMessages(unicornManagementResource);
+            requestForQuotesTopic.addSubscription(new subscription.LambdaSubscription(unicornManagementResource));
+
+        }
+        const sqsJobQueue = new sns.Topic(this, `UnicornManagementResourceSqsJob`);
+        sqsJobQueue.addSubscription(new sqsSubscriptions.SqsSubscription(requestForQuotesResponseQueue));
+
+        // QuotesResponseService
+        const quotesResponseService = new lambda.Function(this, 'QuotesResponseService', {
+            runtime: lambda.Runtime.PYTHON_3_7,
+            code: lambda.Code.fromAsset('lambda'),
+            handler: 'quotesResponseService.handler',
+            environment: {
+                TABLE_NAME: ridesBookingTable.tableName,
+            },
+        });
+
+        ridesBookingTable.grantReadWriteData(quotesResponseService);
+
+
+    }
+}
